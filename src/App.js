d99adeb7cf74dab52dff573044ea21271fd6f117
Fluffy-Lion <leoncooper31@gmail.com>
2022-03-22 14:06:13 +0000

    start

--- src/App.js
+++ src/App.js
@@ -6,28 +6,14 @@ import Card from './Card';
 const App = () => {
 
   const [data, setData] = useState("")
-  const handleFetch = async () => {
-    try {
-      let response = await fetch("https://techy-api.vercel.app/api/json")
-      if(response.status !== 200){
-        throw new Error("oops")
-      }
-      let info = await response.json()
-      setData(info)
-    } catch (error){
-      console.log(error.message)
-    }
-  }
-  useEffect(() => {
-    handleFetch()
-  }, [])
+  
   return (
-    <div style={{ border: "dotted orange 4px" }}>
+    <div >
       <h1>header update</h1>
       <h2>hello from header branch</h2>
       <div>
-        <button onClick={handleFetch}>click meh</button>
-        <Card data={data}/>
+        <button>click meh</button>
+        
       </div>
     </div>
   )
