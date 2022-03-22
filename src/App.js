a9927093a795d9fb2640113ec1c147c03f820d2f
leon cooper <leon.cooper@wearecodenation.com>
2022-03-22 13:16:24 +0000

    added from leon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './Card';
 
 const App = () => {
+
+  const [data, setData] = useState("")
+  const handleFetch = async () => {
+    try {
+      let response = await fetch("https://techy-api.vercel.app/api/json")
+      if(response.status !== 200){
+        throw new Error("oops")
+      }
+      let info = await response.json()
+      setData(info)
+    } catch (error){
+      console.log(error.message)
+    }
+  }
+  useEffect(() => {
+    handleFetch()
+  }, [])
   return (
     <div>
       <h1>header update</h1>
       <h2>hello from header branch</h2>
       <div>
-        <button>click meh</button>
-        <p>
-          add some new content for content
-        </p>
+        <button onClick={handleFetch}>click meh</button>
+        <Card data={data}/>
       </div>
     </div>
   )
